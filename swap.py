931520f8b6482e9502acafa4eec2f85754a55361
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-10-27 13:45:42 -0700

    implemented if statement to return an empty string if it is empty, or esle you get an error

--- swap.py
+++ swap.py
@@ -1,6 +1,9 @@
+
 def swap_list(aList):
     x = len(aList) - 1 #last postition
     y = x//2 #index in middle 
-    aList[x],aList[y]= aList[y],aList[x]
-    return aList 
-    #print(aList)
+    if y <= 0:#if list is empty 
+        return aList
+    else:
+        aList[x],aList[y]= aList[y],aList[x]
+        return aList 

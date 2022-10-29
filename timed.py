359630d922eb1e511cf5d41cb306c5a9e1f53c33
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-10-29 14:12:16 -0700

    first attempt at time decorator

--- timed.py
+++ timed.py
@@ -1,5 +1,12 @@
-import time 
+from time import time 
 
-@timeme 
+def timeme(func):#define decorator functio 
+	def wrapper(time1,time2):#insinde of wrapper actaully does something
+		t0=time.time()
+		call_func = func(time1,time2)
+		t1=time.time()
+		print (f"Total time {(t1-t0)}")
+		return call_func 
+	return wrapper
 
 

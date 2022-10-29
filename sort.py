a8366414a732a6703cb7a99eb7c477711f07c10c
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-10-29 14:41:24 -0700

    added comments for further analysis

--- sort.py
+++ sort.py
@@ -1,9 +1,9 @@
 def sort_dictionary(aDict):
-    sorted_names =[]
-    sorted_phonenum =[]
-    dict_copy=sorted(aDict.items(), key=lambda x: x[1][1])
+    sorted_names =[]#list of name
+    sorted_phonenum =[]#list of phonenumbers
+    dict_copy=sorted(aDict.items(), key=lambda x: x[1][1])#sort by age
     for i in dict_copy:
-        sorted_names.append(i[0])
-        sorted_phonenum.append(i[1][0])
-    complete_list_tuple = list(zip(sorted_names,sorted_phonenum))
+        sorted_names.append(i[0])#add names to list
+        sorted_phonenum.append(i[1][0])#add phone numbers to list 
+    complete_list_tuple = list(zip(sorted_names,sorted_phonenum))#zip creates a list(of tuples)
     return complete_list_tuple

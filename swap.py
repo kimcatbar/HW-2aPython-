d99f732ade3a5bc5050f98d583e84d63235d60e8
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-10-31 13:07:38 -0700

    if list = 1, just return it

--- swap.py
+++ swap.py
@@ -2,7 +2,7 @@
 def swap_list(aList):
     x = len(aList) - 1 #last postition
     y = x//2 #index in middle 
-    if y <= 0:#if list is empty 
+    if y <= 1:#if list is empty 
         return aList
     else:
         aList[x],aList[y]= aList[y],aList[x]

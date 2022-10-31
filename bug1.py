c39735e1aad73770e7c4f3ceea1c9cd898f5e5a5
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-10-30 20:19:49 -0700

    redid the problem, di not understand the first time around

--- bug1.py
+++ bug1.py
@@ -1,27 +1,66 @@
-class Base:
+
+class Base():
     def __init__(self, x, y, size):
-    # TODO: will need to fill this in
-        self.x=x
-        self.y=y
-        self.size=size
-    def draw(self): 
+        # TODO: will need to fill this in
+        self.x = x
+        self.y = y
+        self.size = size
+        
+    def draw(self):
         return ""
 
+class Circle(Base):
+    def __init__(self, x, y, size):
+        super().__init__(x, y, size)
+
+    def draw(self):
+        return f"""
+({self.x}, {self.y})
+{self.size}
+        , - ~ ~ ~ - ,
+    , '               ' ,
+  ,                      ,
+ ,                        ,
+,                          ,
+,                          ,
+,                          ,
+ ,                        ,
+  ,                      ,
+    ,                 , '
+      ' - , _ _ _ , '
+              """
 
 class Square(Base):
     def __init__(self, x, y, size):
-        super().__init__(x,y,size)
-    def draw(self): 
-        output = "\n" * self.y  +  " " * self.x  + "-" * ((self.size*7)+2) + "\n"
-        for _ in range (0,(self.size*3)):
-            output += " " * self.x + "|" + " " * (self.size*7) + "|" + "\n"
-        output += " " * self.x + "-" * ((self.size*7)+2)
-        return output 
-def draw_any_shape(myShape): 
+        super().__init__(x, y, size)
+        
+    def draw(self):
+        return f"""
+({self.x}, {self.y})
+{self.size}
+
+--------------------
+|                  |
+|                  |
+|                  |
+|                  |
+|                  |
+|                  |
+|                  |
+|                  |
+--------------------
+"""
+
+# All of the code below is correct
+def draw_any_shape(myShape):
     print(myShape.draw())
+
 def main():
-    s = Square(1,2,3) 
+    s = Square(1,2,3)
     draw_any_shape(s)
-    #c = Circle(2,2,1)
-   # draw_any_shape(c)
+
+    c = Circle(2,2,1)
+    draw_any_shape(c)
+
 main()
+

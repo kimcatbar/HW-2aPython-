bf13f1cedec9b74a203a3f34426ee058cd891bbc
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-11-01 14:41:21 -0700

    renamed decorator function and removed function that call decorator/func within decorator

--- timed.py
+++ timed.py
@@ -1,6 +1,6 @@
 import time 
 
-def timer_func(func):
+def timeme(func):
     def wrapper():
         t1 = time.time()
         func()
@@ -8,9 +8,9 @@ def timer_func(func):
         print(f'Total time {(t2-t1)}')
     return wrapper
   
-@timer_func #this in leu of calling test_time = ........
-def test_time():
-   time.sleep(2)
+#@timme this in leu of calling test_time = ........
+#def test_time():
+  # time.sleep(2)
    
 #test_time = timer_func(test_time)
-test_time()
+#test_time()

505a380e3e09210d067d1cd8c94d15b0cdf09a33
Kimberly Manzano <kimberly.manzano@sjsu.com>
2022-11-03 13:49:07 -0700

    corrected: if list has two elements

--- swap.py
+++ swap.py
@@ -2,7 +2,7 @@
 def swap_list(aList):
     x = len(aList) - 1 #last postition
     y = x//2 #index in middle 
-    if y <= 1:#if list is empty 
+    if y < 0:#if list only has one elememt since o//2 = 0 
         return aList
     else:
         aList[x],aList[y]= aList[y],aList[x]
